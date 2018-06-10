402d20016068c90a468b50d8e3c8871bafae4f8b
Bhawesh Singh <sbhaweshsingh@gmail.com>
2018-06-10 12:07:12 +0530

    Adding update script

---
+++ mg_d/mongodb-update.js
@@ -0,0 +1,28 @@
+//const MongoClient = require('mongodb').MongoClient;
+
+const {MongoClient,ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017',(err,client) => {
+     if(err)
+     {
+     	return console.log("unable to connect");
+     }
+     console.log("connected to Mongo Server");
+
+     var db = client.db('TodoApp');
+     
+     db.collection('Users').findOneAndUpdate({_id:new ObjectID('5b1a67c3285beb11b06b6453')
+      },{
+        $set : {
+            Name : "Bhawesh Singh"
+        },
+        $inc : {
+            roll : 1
+        }
+      },{returnOriginal : false}).then((result) => {
+         console.log(result);
+      });
+
+     client.close();
+
+});
