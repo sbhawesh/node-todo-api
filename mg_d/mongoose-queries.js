00d0073b928a18a312b24a751b5d7f3d505073ef
sbhawesh <31862771+sbhawesh@users.noreply.github.com>
2020-04-18 12:05:28 +0530

    added mongoose queries

--- mg_d/mongoose-queries.js
+++ mg_d/mongoose-queries.js
@@ -4,6 +4,7 @@ const {mongoose} = require('./../server/db/mongoose');
 const {Todo} = require('./../server/models/todo');
 
 var id = '5b262a030548a4081cfd1fe5';
+var id1 = '5b262a030548a4081cfd1fe3';
 
 if(!ObjectID.isValid(id)){
 	console.log('ID not valid');
@@ -15,3 +16,10 @@ Todo.findById(id).then((todo) => {
 	}
 	console.log(JSON.stringify(todo,undefined,2));
 }).catch((e) => console.log(e));
+
+//Todo.findById(id1).then((todo) => {
+//	if(!todo){
+//		return console.log("ID not found");
+//	}
+//	console.log(JSON.stringify(todo,undefined,2));
+//}).catch((e) => console.log(e));
