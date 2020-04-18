609fa21e3f1b26c6b58925551be69399fe943b46
sbhawesh <31862771+sbhawesh@users.noreply.github.com>
2020-04-18 12:01:28 +0530

    Add DELETE/todo/:id route and test cases

--- mg_d/mongodb-remove.js
+++ mg_d/mongodb-remove.js
@@ -3,13 +3,14 @@ const {ObjectID} = require('mongodb');
 const {mongoose} = require('./../server/db/mongoose');
 const {Todo} = require('./../server/models/todo');
 
+Todo.findOneAndRemove({_id:'5b30a3cba69d81198847037f'}).then((result) => {
+   console.log(result);
+});
 // Todo.remove({}).then((res) => {
 //     console.log(res);
 // });
 
-Todo.findOneAndRemove({_id:'5b30a3cba69d81198847037f'}).then((result) => {
-   console.log(result);
-});
+
 
 // Todo.findByIdAndRemove('5b30a3cba69d81198847037f').then((result) => {
 //    console.log(result);
