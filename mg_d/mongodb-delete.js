c10298adfe24cebbd86b9e7e55e57f6571d32f16
sbhawesh <31862771+sbhawesh@users.noreply.github.com>
2020-04-18 11:58:35 +0530

    updated delete

--- mg_d/mongodb-delete.js
+++ mg_d/mongodb-delete.js
@@ -4,12 +4,13 @@ const {MongoClient,ObjectID} = require('mongodb');
 
 MongoClient.connect('mongodb://localhost:27017',(err,client) => {
      if(err)
-     {
-     	return console.log("unable to connect");
-     }
+          return console.log("unable to connect");
      console.log("connected to Mongo Server");
 
      var db = client.db('TodoApp');
+     db.collection('Todos').findOneAndDelete({completed:true}).then((result) => {
+        console.log(result);
+      });
      
      // db.collection('Todos').deleteMany({text:"creating duplicates"}).then((result) => {
      //  console.log(result);
@@ -17,9 +18,7 @@ MongoClient.connect('mongodb://localhost:27017',(err,client) => {
 
      //db.collection('Todos').deleteOne({_id:new ObjectID('5b1bcf7204c071a02139913e')});
 
-      db.collection('Todos').findOneAndDelete({completed:true}).then((result) => {
-        console.log(result);
-      });
+      
 
      client.close();
 
